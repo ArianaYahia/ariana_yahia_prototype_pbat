e8debd1759dc6fd521c7c1320f7acffc5f7fba1c
ArianaYahia <72752550+ArianaYahia@users.noreply.github.com>
2021-04-27 12:45:17 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,6 @@
+while True:
+print(input.acceleration(Dimension.X))
+if input.acceleration(Dimension.X) > 17 or input.acceleration(Dimension.X) < -17:
+    light.show_animation(light.rainbow_animation, 100)
+else:
+    light.clear()
