7196b9190d94b5bfc73420e341f35987b96782a9
ArianaYahia <72752550+ArianaYahia@users.noreply.github.com>
2021-04-27 13:02:59 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 while True:
-    print(input.acceleration(Dimension.X))
-    if input.acceleration(Dimension.X) > 17 or input.acceleration(Dimension.X) < -17:
-        light.show_animation(light.rainbow_animation, 100)
-    else:
+    print(input.rotation(Rotation.ROLL))
+    if input.rotation(Rotation.ROLL) > 18 or input.rotation(Rotation.ROLL) < -18:
         light.clear()
+    else:
+        light.show_animation(light.rainbow_animation, 100)
+        music.magic_wand.play_until_done()
